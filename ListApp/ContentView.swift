593dbcf1a5c0d6517503ae7c17bc06615b59f47d
DaikiMutaguchi <mucchi43@outlook.jp>
2020-12-10 15:48:49 +0900

    Initial Commit

---
+++ ListApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ListApp
+//
+//  Created by Daiki Mutaguchi on 2020/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(0 ..< 10) { item in
+                NavigationLink(destination: Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate") }
+                
+            }.navigationTitle("タイトル")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by Daiki Mutaguchi on 2020/12/01.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
