4e149aa1726134e73ba3abb7e35baf08f7a21a22
DaikiMutaguchi <mucchi43@outlook.jp>
2020-12-10 16:02:30 +0900

    テストコミット

--- ListApp/ContentView.swift
+++ ListApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             List(0 ..< 10) { item in
                 NavigationLink(destination: Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate") }
                 
-            }.navigationTitle("タイトル")
+            }.navigationTitle("変更確認")
         }
     }
 }
